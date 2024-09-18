02378a18b9495a4173e7006f093a35305b5a8fc0
IT24102131 <it24102131@my.sliit.lk>
2024-09-19 04:57:42 +0530

    Add Q2A,Q2B,Q2C

---
+++ Q2/IT24102131Lab7Q2C.java
@@ -0,0 +1,9 @@
+public class IT24102131Lab7Q2C{
+	public static void main (String[]args){
+		for(int i=5;i>0;i--){
+			for(int y=1;y<=i;y++){
+			System.out.print(i);}
+		System.out.println();}
+	}
+}
+		
